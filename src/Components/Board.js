3a357f057f35d487472c16878745fb089ddb8ce8
Luca <pereluca10@gmail.com>
2024-03-01 18:56:21 -0500

    Board and square logic finished

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -6,30 +6,82 @@ function Board() {
 	const verticalAxis = ['1', '2', '3', '4', '5', '6', '7', '8'];
 	const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-	const [coordinateToGuess, setCoordinateToGuess] = useState('');
-	const handleNewSquare = useCallback(() => {
-		setCoordinateToGuess((a) => a + 'a');
-	}, []);
+	const [coordinateToGuess, setCoordinateToGuess] = useState('a1');
+	const [highscore, setHighscore] = useState(0);
+	const [score, setScore] = useState(0);
+
+	const handleNewSquare = useCallback(
+		(e) => {
+			setCoordinateToGuess((a) => a + 'a');
+
+			if (e.target.id === coordinateToGuess) {
+				setScore((i) => i + 1);
+			} else {
+				setScore(0);
+				if (score > highscore) setHighscore(score);
+			}
+
+			const generateNewSquare = () => {
+				return (
+					horizontalAxis[Math.floor(Math.random() * horizontalAxis.length)] +
+					verticalAxis[Math.floor(Math.random() * verticalAxis.length)]
+				);
+			};
+			setCoordinateToGuess(generateNewSquare);
+		},
+		// probably not good practice but it doesnt break anything.
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[coordinateToGuess, highscore, score]
+	);
 
 	for (let j = verticalAxis.length - 1; j >= 0; j--) {
 		for (let i = 0; i < horizontalAxis.length; i++) {
 			const number = j + i + 2;
+			const key = horizontalAxis[i] + verticalAxis[j];
 			board.push(
 				<Square
+					key={key}
 					colour={number % 2 === 0 ? 'dark' : 'light'}
-					coordinate={horizontalAxis[i] + verticalAxis[j]}
+					coordinate={key}
 					handleNewSquare={handleNewSquare}
 				/>
 			);
 		}
 	}
-	return <div id="board-container">{board}</div>;
+	return (
+		<>
+			<div id="body-container">
+				<div id="board-container">{board}</div>
+				<ScoreCard
+					score={score}
+					highscore={highscore}
+					coordinateToGuess={coordinateToGuess}
+				/>
+			</div>
+		</>
+	);
 }
 
 function Square({ colour, coordinate, handleNewSquare }) {
 	return (
-		<div className={`${colour} square`} onClick={handleNewSquare}>
-			{coordinate}
+		<div
+			id={coordinate}
+			className={`${colour} square`}
+			onClick={(e) => handleNewSquare(e)}
+		></div>
+	);
+}
+
+function ScoreCard({ score, highscore, coordinateToGuess }) {
+	return (
+		<div id="scorecard-container">
+			<div>
+				<h1 className="header-text">Find: {coordinateToGuess}</h1>
+				<h1 className="header-text">Highscore</h1>
+				<p className="text">{highscore}</p>
+				<h1 className="header-text">Current Score</h1>
+				<p className="text">{score}</p>
+			</div>
 		</div>
 	);
 }
