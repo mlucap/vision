7b6d605cfb1320f648034779d456a6f4aad62554
Luca <pereluca10@gmail.com>
2025-01-18 16:00:57 -0500

    update icon styling

--- src/Components/Icons.js
+++ src/Components/Icons.js
@@ -4,7 +4,7 @@ import '../css/Icons.css'
 
 const Icons = ({ colour }) => {
 	return (
-		<div id="icon-container">
+		<div className="icon-container">
 			<img src={colour === "white" ? b_400 : w_400} id="top" alt="Player icon" className="icon" />
 			<img src={colour === "white" ? w_400 : b_400} id="bottom" alt="Player icon" className="icon" />
 		</div>
